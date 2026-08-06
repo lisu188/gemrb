c0d23fea8c319946891b252322a9a96a81f15c4b
lisu188 <andrzej.lis3@gmail.com>
2026-08-06 15:26:04 +0200

    Extract Android managed config generation

---
+++ platforms/android/app/src/main/java/org/gemrb/gemrb/ManagedConfig.java
@@ -0,0 +1,34 @@
+package org.gemrb.gemrb;
+
+import java.io.File;
+
+final class ManagedConfig {
+    private ManagedConfig() {
+    }
+
+    static String build(
+            File runtime,
+            File gamePath,
+            File savePath,
+            File cachePath,
+            String gameType
+    ) {
+        File gemrbData = new File(runtime, "gemrb");
+        return "GameType=" + gameType + "\n"
+                + "GamePath=" + gamePath.getAbsolutePath() + "\n"
+                + "GemRBPath=" + gemrbData.getAbsolutePath() + "\n"
+                + "GUIScriptsPath=" + gemrbData.getAbsolutePath() + "\n"
+                + "GemRBOverridePath=" + gemrbData.getAbsolutePath() + "\n"
+                + "GemRBUnhardcodedPath=" + gemrbData.getAbsolutePath() + "\n"
+                + "SavePath=" + savePath.getAbsolutePath() + "\n"
+                + "CachePath=" + cachePath.getAbsolutePath() + "\n"
+                + "AudioDriver=none\n"
+                + "Logging=1\n"
+                + "SkipIntroVideos=1\n"
+                + "TouchInput=1\n"
+                + "MouseFeedback=3\n"
+                + "CaseSensitive=1\n"
+                + "Width=640\n"
+                + "Height=480\n";
+    }
+}
