ec72e11efe821b2cd0780f163a36e08e7aa2ed28
lisu188 <andrzej.lis3@gmail.com>
2026-08-06 13:31:09 +0200

    android: pass managed config to native GemRB

--- platforms/android/app/src/main/java/org/gemrb/gemrb/GemRBActivity.java
+++ platforms/android/app/src/main/java/org/gemrb/gemrb/GemRBActivity.java
@@ -2,5 +2,12 @@ package org.gemrb.gemrb;
 
 import org.libsdl.app.SDLActivity;
 
+import java.io.File;
+
 public final class GemRBActivity extends SDLActivity {
+    @Override
+    protected String[] getArguments() {
+        File config = new File(getFilesDir(), "config/GemRB.cfg");
+        return new String[] { "-c", config.getAbsolutePath() };
+    }
 }
